758d99192c156c6493795b692646659bf5106ad3
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2026-03-02 18:22:15 +0100

    Scripts RPM

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const botonAplicar = document.getElementById("btn-aplicar")
+const botonAplicar = document.getElementById("btn-aplicar");
+const inputRpm = document.getElementById("input-rpm");
+
+botonAplicar.addEventListener("click", function () {
+  console.log("Ajustando velocidad a: " + inputRpm.value + " RPM");
+});
